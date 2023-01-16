8c42e1484dec7e57105a2d758d0d930ecd69653d
SubhamDey29 <118063699+SubhamDey29@users.noreply.github.com>
2023-01-16 09:50:00 +0000

    Create action.groovy

---
+++ vars/action.groovy
@@ -0,0 +1,11 @@
+
+def call()
+    {
+        if(params.Action=='Build'){
+        sh("terraform apply -auto-approve");
+        }
+        else if(params.Action=='Destroy'){
+        sh("terraform destroy -auto-approve");
+        }
+    }
+       
